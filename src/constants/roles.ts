dc682cd69213511fd5b5bd45f39e880f706d45ab
Philip Adelabu <adebisiphilip@gmail.com>
2026-08-01 11:30:10 +0100

    Added some config files

---
+++ src/constants/roles.ts
@@ -0,0 +1,5 @@
+export const ROLES = {
+  ADMIN: "ADMIN",
+  CUSTOMER: "CUSTOMER",
+  SELLER: "SELLER",
+} as const;
---
+++ src/constants/site.ts
@@ -0,0 +1,12 @@
+export const SITE_CONFIG = {
+  name: "Handcrafted Haven",
+
+  description:
+    "A marketplace connecting talented artisans with customers around the world.",
+
+  url: "https://handcraftedhaven.com",
+
+  ogImage: "/images/og-image.png",
+
+  creator: "Philip Adelabu",
+};
